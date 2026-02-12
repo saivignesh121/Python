c79f7df2e85900714db4641790c4d601b70ab525
saivignesh121 <vigneshkolu@gmail.com>
2026-02-12 11:02:57 +0530

    Add files via upload
    Python Introduction

---
+++ day1-Basics of python.py
@@ -0,0 +1,4 @@
+print("this is Sai vignesh first day at codegnan")
+print("Today i Learnt Introduction about Python")
+print("i learnt python features such as interpreter High Level Oop Multi ParadigmLanguage")
+
