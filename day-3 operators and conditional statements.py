e522db89cde552ab8f5909b4971783f6ac29785f
saivignesh121 <vigneshkolu@gmail.com>
2026-02-14 22:07:54 +0530

    Add files via upload
    operators and conditional statements

---
+++ day-3 operators and conditional statements.py
@@ -0,0 +1,39 @@
+#check number is zero 
+num=int(input())
+if (num==0):
+    print("number is zero")
+print("program Done")
+
+#check if number is zero or not
+num=int(input())
+if(num==0):
+    print("number is zero")
+else:
+    print("number not zero")
+print("program Done")
+
+
+# same prog by not equal to
+num=int(input())
+if (num!=0):
+    print("number is not zero")
+else:
+    print("number is zero")
+print("program done")
+
+#print the biggest number of two
+val1=int(input())
+val2=int(input())
+if (val1>val2):
+    print("val1 is bigger")
+else:
+    print("val2 is bigger")
+
+#another case of printing the bigger number
+val1=int(input())
+val2=int(input())
+if val1>val2:
+    print("val1 is bigger")
+if val2>val1:
+    print("val2 is bigger")
+    
